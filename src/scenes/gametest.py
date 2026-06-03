56fd4094ba4c577dd31eb5679399f845049a9199
Mesut <mesutahmettravaci@gmail.com>
2026-06-04 00:22:36 +0300

    fix the broken
    * update github
    
    * update gitignore
    
    * m
    
    * fix mostly

--- src/scenes/gametest.py
+++ src/scenes/gametest.py
@@ -2,12 +2,18 @@ import pygame as pg
 from util.button import Button
 from scenes.scene import Scene
 
-class OptionsScene(Scene):
+class GameTest(Scene):
     def __init__(self, width: int, height: int, game):
         super().__init__("wizardshut - options", width, height, game)
         self.button1 = Button(50, 50, 200, 70, "back to menu", lambda: game.changeSceneTo("MainMenu"))
-        self.oscarimg = pg.image.load('src/resources/img/oscar.png')
         self.mesutimg = pg.transform.scale(pg.image.load('src/resources/img/mesut.png'), (100, 140))
+
+    def update(self, game):
+        game.updateMouse()
+        game.checkEvents()
+        self.draw(game.mousePos, game.mouseStatus)
+        pg.display.update()
+        game.clock.tick(60)
 
     def draw(self, mousePos, mouseStatus):
         self.window.fill(color="black")
@@ -15,5 +21,3 @@ class OptionsScene(Scene):
         mPos, mStat = self.button1.process(mousePos, mouseStatus)
         self.window.blit(mPos, mStat)
 
-        self.window.blit(self.oscarimg,(50, 50))
-        self.window.blit(self.mesutimg,(200, 200))
