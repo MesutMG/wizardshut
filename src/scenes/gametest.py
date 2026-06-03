639d453199ed1e458d429f03641a5f370feb77a0
Mesut <mesutahmettravaci@gmail.com>
2026-06-04 00:40:17 +0300

    Implement character movement controls in gametest
    Added movement controls for the character 'mesut' using arrow keys.

--- src/scenes/gametest.py
+++ src/scenes/gametest.py
@@ -7,17 +7,41 @@ class GameTest(Scene):
         super().__init__("wizardshut - options", width, height, game)
         self.button1 = Button(50, 50, 200, 70, "back to menu", lambda: game.changeSceneTo("MainMenu"))
         self.mesutimg = pg.transform.scale(pg.image.load('src/resources/img/mesut.png'), (100, 140))
+        self.mesutpos = [100,100]
+        
+    def checkEvents(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.game.running = False
+        
+            if event.type == pg.KEYDOWN:    
+                if event.key == pg.K_q:
+                    self.game.running = 0
+                
+                if event.key == pg.K_UP:
+                    self.mesutpos[1] -= 10
+                
+                if event.key == pg.K_DOWN:
+                    self.mesutpos[1] += 10
+                
+                if event.key == pg.K_RIGHT:
+                    self.mesutpos[0] += 10
 
-    def update(self, game):
-        game.updateMouse()
-        game.checkEvents()
-        self.draw(game.mousePos, game.mouseStatus)
+                if event.key == pg.K_LEFT:
+                    self.mesutpos[0] -= 10
+
+    def update(self):
+        self.game.updateMouse()
+        self.checkEvents()
+        self.draw(self.game.mousePos, self.game.mouseStatus)
         pg.display.update()
-        game.clock.tick(60)
+        self.game.clock.tick(60)
 
     def draw(self, mousePos, mouseStatus):
         self.window.fill(color="black")
 
         mPos, mStat = self.button1.process(mousePos, mouseStatus)
         self.window.blit(mPos, mStat)
+
+        self.window.blit(self.mesutimg,(self.mesutpos[0], self.mesutpos[1]))
 
