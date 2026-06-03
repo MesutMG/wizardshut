445bffa3b18e8acc2ef79497548fba9ac5e31aae
mesutmg <mesutahmettravaci@gmail.com>
2026-06-04 00:19:32 +0300

    fix mostly

--- src/scenes/scene.py
+++ src/scenes/scene.py
@@ -6,6 +6,14 @@ class Scene:
         self.window = pg.display.set_mode((width, height), vsync=1)
         self.game = game
 
+    def update(self, game):
+        game.updateMouse()
+        game.checkEvents()
+        self.draw(game.mousePos, game.mouseStatus)
+        pg.display.update()
+        game.clock.tick(60)
+
     def draw(self, mousePos: tuple[int ,int], mouseStatus: tuple[bool, bool, bool]):
         self.window.fill(color="blue")
+
         
